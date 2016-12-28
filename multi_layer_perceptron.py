07a6473471f2e83a6afbe64428d3ddd6bb9ee87c
Allyaenn <alix.gonnot@gmail.com>
2016-12-28 16:57:52 +0100

    adaptation du forward pour plusieurs couches de neurones

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -4,69 +4,79 @@ import numpy # pour pouvoir utiliser des matrices
 import pylab # pour l'affichage
 
 #paramètrage
-nbStep = 2;
-nbLayer = 1;
-learningRate = 0.5;
+nbStep = 2; #nombre d'itérations
+nbLayer = 5; #nombre de couches dans le réseau de neurones
+nbNeurons = 10; #nombre de neurones par couche
+learningRate = 0.1; #taux d'apprentissage
 
 #chargement de la base de données.
 f = gzip.open('mnist.pkl.gz')
 data = pickle.load(f,encoding='latin1')
 
 #comment naviguer dans la base de données ?
-# (data[0][0][1])
-# test ou apprentissage
-# image ou label
-#numéro de l'exemple d'apprentissage
+# 1ère colonne : test ou apprentissage
+# 2ème colonne : image ou label
+# 3ème colonne : numéro de l'exemple d'apprentissage
 
 #selection d'une entrée au hasard dans la base
-
-#print (data[0][1][0][0])
 
 vec = data[0][0][0]
 vec = numpy.append(vec, 1)
 
 #intialisation de la structure de données
-weight_matrix_list = [nbLayer]
+weight_matrix_list = []
 for i in range (0, nbLayer) :
     if i == 0 :
-        weight_matrix_list[i] = numpy.zeros((vec.size,10))
+        weight_matrix_list.append(numpy.zeros((nbNeurons,vec.size)))
     else :
-        weight_matrix_list[i] = numpy.zeros((11,10))
+        weight_matrix_list.append(numpy.zeros((nbNeurons,nbNeurons + 1))) #1 ligne contient les poids qui permette de construire le neurone d'arrivée
 #TODO : initialiser les poids de façon random
 
-print ("Weight Matrix List : ")
-print (len(weight_matrix_list))
-
-#on commence avec une seule couche de 10 neurones pour commencer
-out = numpy.empty((10,1))
+#déclaration des structures qui vont stocker les valeurs des neurones
+out = numpy.empty((0,0))
+current_tmp = numpy.empty((0,0))
+prior_tmp = numpy.empty((0,0))
 
 for i in range (0,nbStep) : # sur n pas de temps
-    for j in range (0,nbLayer) : #pour chaque couche du réseau
-        #for k in range (0,10) : #pour chaque neurone de la couche
-            #application de la fonction d'activation
+    for j in range (0,nbLayer) : #pour chaque couche du réseau application de la fonction d'activation
             #TODO : structuration du calcul en fonction du nombre de couche
-            out = numpy.dot(numpy.transpose(weight_matrix_list[j]), vec) #le produit matriciel remplace la somme
-            out = 1/(1 + numpy.exp(-out))
-            print(out)
-            #attention au biais -> ajouter 1 à la fin du tableau si ce n'est pas la dernière couche
+            if j == 0 : #si on est sur la première couche, on prend l'entrée vec
+                print ("IF 1 - Premier cas")
+                print(vec.shape)
+                print (weight_matrix_list[j].shape)
+                current_tmp = numpy.dot(weight_matrix_list[j], vec) #le produit matriciel remplace la somme
+                current_tmp = 1/(1 + numpy.exp(-current_tmp))
+                print (current_tmp.shape)
+            else : #sinon, on prend les données calculées dans la couche précedente
+                print ("IF 1 - Deuxieme cas")
+                print ("WML : ")
+                print (weight_matrix_list[j].shape)
+                print ("PRIOR")
+                print (prior_tmp.shape)
+                current_tmp = numpy.dot(weight_matrix_list[j], prior_tmp) #le produit matriciel remplace la somme
+                current_tmp = 1/(1 + numpy.exp(-current_tmp))
+                print ("CURRENT")
+                print (current_tmp.shape)
+
+            if j == nbLayer-1 : #si on est sur la dernière couche, on est sur la sortie -> pas d'ajout de biais, mise dans out
+                print ("IF 2 - Premier cas")
+                out = current_tmp
+                print(out)
+            else : #attention au biais -> ajouter 1 à la fin du tableau si ce n'est pas la dernière couche
+                print ("IF 2 - Deuxieme cas")
+                prior_tmp = numpy.append(current_tmp,1)
+                print ("CURRENT")
+                print (current_tmp)
+                print ("PRIOR")
+                print (prior_tmp)
 
     #calcul de l'erreur pour la couche de sortie
-    tab = numpy.empty(out.shape)
-    print(out.size)
-    print(tab.size)
-    for k in range (0,out.size) :
-        tab[k] = out[k]*(1-out[k])*(data[0][1][0][k]-out[k])
+    #tab = numpy.empty(out.shape)
+    #print(out.size)
+    #print(tab.size)
+    #for k in range (0,out.size) :
+        #tab[k] = out[k]*(1-out[k])*(data[0][1][0][k]-out[k])
 
-    print (tab)
+    #print (tab)
 
     #retropropagation des poids
-
-
-#pour n pas de temps
-
-#propagation
-
-#calcul de l'erreur et back propagation
-
-
-#C'est quoi les poids, on commence à 0 ?
